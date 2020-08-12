265d1f377c902dc8d1023d02fabe2ef93ba1bdde
Sébastien Levert <slevert@outlook.com>
2020-08-12 10:58:14 -0400

    Splitted the Templates and utilizing 100% template

--- samples/video-hub-site/Provision-ValoTemplate.ps1
+++ samples/video-hub-site/Provision-ValoTemplate.ps1
@@ -40,9 +40,13 @@ Param(
     [ValidateNotNullOrEmpty()]
     [String]$SiteTitle,
     
+    [Parameter(Mandatory = $false, HelpMessage = "File path to the Valo Templates pnp template(s) used for this provisioning process")]
+    [ValidateNotNullOrEmpty()]
+    [String]$ValoTemplatesPnPPath = ".\ValoTemplates\Provisioning\*.xml",
+
     [Parameter(Mandatory = $false, HelpMessage = "File path to the pnp template(s) used for this provisioning process")]
     [ValidateNotNullOrEmpty()]
-    [String]$PnPPath = ".\ProvisioningTemplates\*.xml",
+    [String]$TemplatesPnPPath = ".\Templates\Provisioning\*.xml",
 
     [Parameter(Mandatory = $false, HelpMessage = "Name of Windows Credentials key used for connecting to Office 365 with a saved account.")]
     [String]$Office365CredentialStoreKey,
@@ -60,7 +64,6 @@ Param(
 
     [switch]$useMFA
 )
-
 
 Push-Location $PSScriptRoot
 
@@ -95,8 +98,7 @@ function Get-Framework($PartnerPackScriptRoot = $null) {
     Catch {
         Write-Error "Error getting Valo PartnerPack artifacts from path $PartnerPackScriptRoot. Aborting."
         Exit
-    }
-    
+    }    
 }
 
 function Get-ValoTemplatesUrl {
@@ -135,33 +137,6 @@ function Get-ValoTemplatesUrl {
 
     return $TemplateSiteUrl
 
-}
-
-function Add-FolderStructure() {
-    Param(
-        $SourceFolder, 
-        $DestinationFolder
-    )
-
-    $RootFolder = Get-Item $SourceFolder
-    
-    Get-ChildItem "$($RootFolder.FullName)" -Recurse | % { 
-    
-        # Check if current item is a folder
-        if ($_.PSIsContainer) {
-            $FolderToCreateParent = "$DestinationFolder$($_.Parent.FullName.Replace($RootFolder.FullName, '').Replace("\", "/"))"
-            if (-not (Get-PnPFolder "$FolderToCreateParent/$($_.Name)" -ErrorAction SilentlyContinue)) {
-                Log -Message "Creating folder $($_.Name) in $FolderToCreateParent on template site." -Level Info
-                Add-PnPFolder -Name $_.Name -Folder $FolderToCreateParent | Out-Null
-            }
-        }
-        else {
-            $FileToCreateParent = "$DestinationFolder$($_.DirectoryName.Replace($RootFolder.FullName, '').Replace("\", "/"))"
-            Log -Message "Uploading file $($_.Name) to $FileToCreateParent on template site." -Level Info
-            Add-PnPFile -Path $_.FullName -Folder $FileToCreateParent | Out-Null
-        }
-    }
-    
 }
 
 function Get-SiteDefaultLanguage {
@@ -214,100 +189,35 @@ function Get-ResourceFileValues {
     return $Values
 }
 
-function Deploy-ValoTemplatesSite {
+function Deploy-ValoTemplate {
     Param(
-        $SiteUrl
+        $SiteUrl,
+        $TemplatePath
     )
 
-    ## Get target site's valo templates site
-    $TemplateSiteUrl = Get-ValoTemplatesUrl -SiteUrl $SiteUrl
+    # Validating the Path and Applying the templates
+    if(Test-Path $TemplatePath) {
+        # Commence updates to site
+        Log -Message "Connecting to site $SiteUrl" -Level Info
+        $siteConnected = Ensure-ValoConnection -Url $SiteUrl
 
-    ## Commence updates to Valo templates site
-    Log -Message "Connecting to Valo Templates site $TemplateSiteUrl" -Level Info
-    $templateSiteConnected = Ensure-ValoConnection -Url $TemplateSiteUrl
-
-    if (!$templateSiteConnected) {
-        Log -Message "Something went wrong while connecting to the template site $TemplateSiteUrl, exiting.." -Level Error 
-        Exit
-    }
-
-    $LanguageInformation = $global:LanguageTable | Where-Object { $_.LCID -eq (Get-SiteDefaultLanguage -Url $TemplateSiteUrl) }
-    $LanguageResources = Get-ResourceFileValues -Path ".\Resources\$($LanguageInformation.LanguageTag).json"
-
-    # Web Part Templates
-    if(Test-Path ".\ValoTemplates\TemplatesGallery\*.html") {
-        Log -Message "Uploading web part templates to Valo Templates Gallery" -Level Info -wrapWithLightSeparator
-        Get-Item .\ValoTemplates\TemplatesGallery\*.html | % {
-            # Check if there's a matching .json file, to use as metadata associated to the upladed html file
-            $CurrentResourceFileName = $_.FullName.Replace(".html", ".json")        
-            $Values = Get-ResourceFileValues -Path $CurrentResourceFileName
-
-            Log -Message "Adding $($_.Name) template to Valo Templates Gallery" -Level Info
-
-            # Upload the HTML file and associated metadata to the TemplatesGallery
-            Add-PnPFile -Path $_.FullName -Folder TemplatesGallery -Values $Values | Out-Null
+        if (!$siteConnected) {
+            Log -Message "Something went wrong while connecting to the site $SiteUrl, exiting.." -Level Error 
+            Exit
         }
-    }
 
-    # Site Assets
-    if(Test-Path ".\ValoTemplates\SiteAssets\*") {
-        Log -Message "Ensuring site assets library exists on site $TemplateSiteUrl" -Level Info -wrapWithLightSeparator
-        Ensure-SiteAssetsLibrary
-        
-        Log -Message "Uploading assets to Valo Templates Site Assets folder" -Level Info -wrapWithLightSeparator
-        Add-FolderStructure -SourceFolder ".\ValoTemplates\SiteAssets" -DestinationFolder SiteAssets
-    }
+        $LanguageInformation = $global:LanguageTable | Where-Object { $_.LCID -eq (Get-SiteDefaultLanguage -Url $SiteUrl) }
+        $LanguageResources = Get-ResourceFileValues -Path ".\Resources\$($LanguageInformation.LanguageTag).json"
 
-    # Page Templates
-    if(Test-Path ".\ValoTemplates\SitePages\*.xml") {
-        Log -Message "Uploading page templates to Valo Templates" -Level Info -wrapWithLightSeparator
-        $PnPTemplates = (Get-ChildItem -Path ".\ValoTemplates\SitePages\*.xml" | Sort-Object -Property Name)
+        Log -Message "Applying provisioning templates to $SiteUrl" -Level Info -wrapWithLightSeparator
+        $PnPTemplates = (Get-ChildItem -Path $TemplatePath | Sort-Object -Property Name)
         
         $PnPTemplates | % { 
-            Log -Message $("Applying page template {0} to the site {1}" -f @($_.Name, $TemplateSiteUrl)) -Level Info
-            Apply-PnPProvisioningTemplate -Path $_.FullName -Parameters $LanguageResources -Debug:$ShowDebug
-        }
-    }
-
-    # Other General Templates
-    if(Test-Path ".\ValoTemplates\ProvisioningTemplates\*") {
-        Log -Message "Uploading templates to Valo Templates" -Level Info -wrapWithLightSeparator
-        $PnPTemplates = (Get-ChildItem -Path ".\ValoTemplates\ProvisioningTemplates\*" | Sort-Object -Property Name)
-        
-        $PnPTemplates | % { 
-            Log -Message $("Applying site template {0} to the site {1}" -f @($_.Name, $TemplateSiteUrl)) -Level Info
+            Log -Message $("Applying template {0} to the site {1}" -f @($_.Name, $SiteUrl)) -Level Info
             Apply-PnPProvisioningTemplate -Path $_.FullName -Parameters $LanguageResources -Debug:$ShowDebug
         }
     }
 }
-
-function Deploy-ValoTargetSite {
-    Param(
-        $SiteUrl
-    )
-    
-    ## Commence updates to target site
-    $connected = Ensure-ValoConnection -url $SiteUrl
-
-    if (!$connected) {
-        Log -Message "Something went wrong while connecting back to the site $SiteUrl, exiting.." -Level Error 
-        Exit
-    }
-
-    $LanguageInformation = $global:LanguageTable | Where-Object { $_.LCID -eq (Get-SiteDefaultLanguage -Url $SiteUrl) }
-    $LanguageResources = Get-ResourceFileValues -Path ".\Resources\$($LanguageInformation.LanguageTag).json"
-
-    if(Test-Path $PnPPath) {
-        Log -Message ("Applying provisioning templates to {0}" -f @($SiteUrl)) -Level Info -wrapWithLightSeparator
-        $PnPTemplates = (Get-ChildItem -Path $PnPPath | Sort-Object -Property Name)
-
-        $PnPTemplates | % { 
-            Log -Message $("Applying site template {0} using language $($LanguageInformation.LanguageTag)" -f @($_.Name, $SiteUrl)) -Level Info
-            Apply-PnPProvisioningTemplate -Path $_.FullName -Parameters $LanguageResources -Debug:$ShowDebug
-        }
-    }
-}
-
 
 #-----------------------------------------------------------------------
 # Loading the Framework
@@ -319,6 +229,12 @@ function Deploy-ValoTargetSite {
 #-----------------------------------------------------------------------
 $TemplateInformation = (Get-Content template.json) | ConvertFrom-Json
 Set-GlobalVariable -parameterName TemplateInformation -value $TemplateInformation
+
+#-----------------------------------------------------------------------
+# Getting the Language Reference Table for further use
+#-----------------------------------------------------------------------
+$LanguageTable = Import-Csv -Path .\Resources\Languages.csv
+Set-GlobalVariable -parameterName LanguageTable -value $LanguageTable
 
 #-----------------------------------------------------------------------
 # Setuping the Logger
@@ -335,7 +251,9 @@ Log -Message $("Provisioning $($TemplateInformation.Name) template") -Level Info
 $tenantUrl = Get-TenantUrlFromSiteUrl $SiteUrl
 Set-GlobalVariable -parameterName tenantUrl -value $tenantUrl
 
+#-----------------------------------------------------------------------
 # Initialize global variables used in the script
+#-----------------------------------------------------------------------
 Initialize-GenesisVariables -tenantUrl $tenantUrl `
     -Office365CredentialStoreKey $Office365CredentialStoreKey `
     -SharePointUserName $SharePointUserName `
@@ -348,74 +266,15 @@ Initialize-Credentials `
     -SharePointPassword $SharePointPassword `
     -useMFA:$useMFA
 
-# Getting the Language Reference Table for further use
-$LanguageTable = Import-Csv -Path .\Resources\Languages.csv
-Set-GlobalVariable -parameterName LanguageTable -value $LanguageTable
-
-# $ResourceFile = Get-ChildItem -Path ".\LanguageResources\$($LCID)_*.xml"
-# if (-not $ResourceFile) { 
-#     $ResourceFile = Get-ChildItem -Path ".\LanguageResources\1033_*.xml" 
-#     Log -Message "Default language for site $SiteUrl isn't available in LanguageResources - defaulting to $($ResourceFile.Name)." -Level Warning
-# }
-# if ($ResourceFile) {
-#     $SiteLanguageResource = Import-Clixml $ResourceFile.FullName
-# }
-# else {
-#     $SiteLanguageResource = @{}
-# }
-
+#-----------------------------------------------------------------------
 # Deploy the Valo Templates artefacts
-Deploy-ValoTemplatesSite -SiteUrl $SiteUrl
+#-----------------------------------------------------------------------
+Deploy-ValoTemplate -SiteUrl (Get-ValoTemplatesUrl -SiteUrl $SiteUrl) -TemplatePath $ValoTemplatesPnPPath
 
+#-----------------------------------------------------------------------
 # Deploy the Target Site artefacts
-Deploy-ValoTargetSite -SiteUrl $SiteUrl
-
-
-# #Possible additional configurations
-# Log -Message $("Executing additional configurations to {0}" -f $SiteUrl) -Level Info -wrapWithSeparator
-
-# if ($SiteTitle) {
-#     Log -Message "  Setting site title to $SiteTitle" -Level Info -wrapWithSeparator
-#     Set-PnPWeb -Title $SiteTitle
-# }
-
-# Log -Message "  Removing content types from the Site Pages library" -Level Info
-
-# $ContentTypesToRemove = @(
-#     '0x01010901' # Web Part Page
-#     '0x0101009D1CB255DA76424F860D91F20E6C4118',  # Site Page
-#     '0x0101009D1CB255DA76424F860D91F20E6C4118002A50BFCFB7614729B56886FADA02339B', # Repost page
-#     '0x0101009D1CB255DA76424F860D91F20E6C41180065789619A4EFB44992AF42CEEBB13C9A02' # Valo News Page
-# )
-
-# $ContentTypesToRemove | % {
-#     Remove-PnPContentTypeFromList -List "Site Pages" -ContentType $_
-# }
-
-# Log -Message "  Renaming content types in the Site Pages library" -Level Info
-
-# $ContentTypesToRename = @(
-#     @{ Id = '0x0101009D1CB255DA76424F860D91F20E6C41180065789619A4EFB44992AF42CEEBB13C9A0301'; Name = "$($SiteLanguageResource.ValoPageTemplateNamePolicy)" }
-#     @{ Id = '0x0101009D1CB255DA76424F860D91F20E6C41180065789619A4EFB44992AF42CEEBB13C9A0302'; Name = "$($SiteLanguageResource.ValoPageTemplateNameProcedure)" }
-#     @{ Id = '0x0101009D1CB255DA76424F860D91F20E6C41180065789619A4EFB44992AF42CEEBB13C9A0303'; Name = "$($SiteLanguageResource.ValoPageTemplateNameHowToGuide)" }
-# )
-
-# $SitePagesList = Get-PnPList "Site Pages"
-# $SitePagesContentTypes = Get-PnPProperty -ClientObject $SitePagesList -Property ContentTypes
-# (Get-PnPContext).Load($SitePagesContentTypes)
-
-# $ContentTypesToRename | % {
-#     $Id = $_.Id;
-#     $Name = $_.Name;
-
-#     $SitePagesContentTypes | ? { $_.Id -like "$Id*" } | % { 
-#         $_.Name = $Name; 
-#         $_.Update($false) 
-#     }
-# }
-
-# (Get-PnPContext).ExecuteQuery()
-
+#-----------------------------------------------------------------------
+Deploy-ValoTemplate -SiteUrl $SiteUrl -TemplatePath $TemplatesPnPPath
 
 $totalRunTime.Stop()
 $total = [Math]::Round($totalRunTime.Elapsed.TotalMinutes).ToString()
